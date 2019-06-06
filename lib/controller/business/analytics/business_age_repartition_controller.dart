1a441a4cda9802abacde2a891eeb687951ee2d60
Ecalle Thomas <thomasecalle@hotmail.fr>
2019-06-06 23:47:28 +0200

    Fix getAges route min age operator

--- lib/controller/business/analytics/business_age_repartition_controller.dart
+++ lib/controller/business/analytics/business_age_repartition_controller.dart
@@ -25,25 +25,30 @@ class BusinessAgeRepartitionController extends ResourceController {
     final firstCount = await _context.persistentStore.execute(first);
     results.add(BusinessCustomersByAgeItemCountResponse(label: "0 - 18", value: firstCount[0][0] as int));
 
-    final second = _getQueryFor(19, 25, account.business.id);
+    final second = _getQueryFor(18, 25, account.business.id);
     final secondCount = await _context.persistentStore.execute(second);
     results.add(BusinessCustomersByAgeItemCountResponse(label: "19 - 25", value: secondCount[0][0] as int));
 
-    final third = _getQueryFor(26, 30, account.business.id);
+    final third = _getQueryFor(25, 30, account.business.id);
     final thirdCount = await _context.persistentStore.execute(third);
     results.add(BusinessCustomersByAgeItemCountResponse(label: "26 - 30", value: thirdCount[0][0] as int));
 
-    final fourth = _getQueryFor(31, 40, account.business.id);
+    final fourth = _getQueryFor(30, 40, account.business.id);
     final fourthCount = await _context.persistentStore.execute(fourth);
     results.add(BusinessCustomersByAgeItemCountResponse(label: "31 - 40", value: fourthCount[0][0] as int));
 
-    final fifth = _getQueryFor(41, 50, account.business.id);
+    final fifth = _getQueryFor(40, 50, account.business.id);
     final fifthCount = await _context.persistentStore.execute(fifth);
     results.add(BusinessCustomersByAgeItemCountResponse(label: "41 - 50", value: fifthCount[0][0] as int));
 
-    final sixth = _getQueryFor(51, 60, account.business.id);
+    final sixth = _getQueryFor(50, 60, account.business.id);
     final sixthCount = await _context.persistentStore.execute(sixth);
     results.add(BusinessCustomersByAgeItemCountResponse(label: "51 - 60", value: sixthCount[0][0] as int));
+
+    final seventh = _getQueryFor(60, 100, account.business.id);
+    final seventhCount = await _context.persistentStore.execute(seventh);
+    results.add(BusinessCustomersByAgeItemCountResponse(label: "61 - +", value: seventhCount[0][0] as int));
+
     return Response.ok(results);
   }
 

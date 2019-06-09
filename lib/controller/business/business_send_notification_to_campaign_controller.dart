f1ba05a417f5a8d13a6137a902ff2680083f2241
Ecalle Thomas <thomasecalle@hotmail.fr>
2019-06-09 18:52:56 +0200

    Made sending notification to campaign works

--- lib/controller/business/business_send_notification_to_campaign_controller.dart
+++ lib/controller/business/business_send_notification_to_campaign_controller.dart
@@ -1,8 +1,15 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
 import 'package:aqueduct/aqueduct.dart';
 import 'package:carl_api/model/account.dart';
+import 'package:carl_api/model/business.dart';
 import 'package:carl_api/model/campaign.dart';
+import 'package:carl_api/model/notification.dart';
+import 'package:carl_api/model/user.dart';
+import 'package:carl_api/response/business_send_notification_response.dart';
+import 'package:http/http.dart' as http;
 
 class BusinessSendNotificationToCampaignController extends ResourceController {
   BusinessSendNotificationToCampaignController(this._context, this.firebaseServerKey);
@@ -17,11 +24,11 @@ class BusinessSendNotificationToCampaignController extends ResourceController {
     @Bind.query("shortDescription") String shortDescription,
     @Bind.query("description") String description,
   ) async {
-    final getBusinessQuery = Query<Account>(_context)
+    final getAccountQuery = Query<Account>(_context)
       ..where((account) => account.id).equalTo(request.authorization.ownerID)
       ..where((account) => account.business).isNotNull();
 
-    final account = await getBusinessQuery.fetchOne();
+    final account = await getAccountQuery.fetchOne();
 
     if (account == null || account.business == null) {
       return Response.unauthorized();
@@ -36,7 +43,7 @@ class BusinessSendNotificationToCampaignController extends ResourceController {
     print("campaign found = ${campaign.name}");
 
     var querySql = """
-    SELECT _user.notificationstoken
+    SELECT _user.notificationstoken, _user.id
     FROM _user
     INNER JOIN _customerrelationship
     ON _customerrelationship.user_id = _user.id
@@ -88,13 +95,69 @@ class BusinessSendNotificationToCampaignController extends ResourceController {
     }
 
     querySql += ";";
-    final List<String> tokens = [];
-    final result = await _context.persistentStore.execute(querySql) as List<List<dynamic>>;
+    final tokensAndIdsList = await _context.persistentStore.execute(querySql) as List<List<dynamic>>;
 
-    result.forEach((internList) {
-      tokens.add(internList[0] as String);
+    final List<Future<User>> usersQueries = [];
+
+    final getBusinessQuery = Query<Business>(_context)
+      ..where((business) => business.id).identifiedBy(account.business.id);
+    final business = await getBusinessQuery.fetchOne();
+
+    tokensAndIdsList.forEach((tokenId) {
+      final userQuery = Query<User>(_context)..where((user) => user.id).identifiedBy(tokenId[1]);
+      usersQueries.add(userQuery.fetchOne());
     });
 
-    return Response.ok(tokens);
+    final users = await Future.wait(usersQueries);
+
+    final List<Future<Notification>> notificationQueries = [];
+    final List<String> tokens = [];
+
+    users.forEach((user) {
+      tokens.add(user.notificationsToken);
+      final insertNotificationQuery = Query<Notification>(_context)
+        ..values.type = NotificationType.simple
+        ..values.title = title
+        ..values.shortDescription = shortDescription
+        ..values.description = description
+        ..values.user = user
+        ..values.business = account.business;
+
+      notificationQueries.add(insertNotificationQuery.insert());
+    });
+
+    await Future.wait(notificationQueries);
+
+    final getUsersThatAreNotOkForNotificationsQuery = """
+          SELECT _user.notificationstoken
+          FROM _user
+          INNER JOIN _notificationsblacklist
+          ON _notificationsblacklist.user_id = _user.id
+          WHERE _notificationsblacklist.business_id = ${business.id};
+        """;
+
+    final doNotDisturbUsersTokens =
+        await _context.persistentStore.execute(getUsersThatAreNotOkForNotificationsQuery) as List<List<dynamic>>;
+
+    doNotDisturbUsersTokens.forEach((token) {
+      tokens.remove(token[0]);
+    });
+
+    final response = await http.post("https://fcm.googleapis.com/fcm/send",
+        headers: {
+          HttpHeaders.authorizationHeader: "key=$firebaseServerKey",
+          HttpHeaders.contentTypeHeader: "application/json"
+        },
+        body: json.encode({
+          "registration_ids": tokens,
+          "notification": {"body": shortDescription, "title": business.name}
+        }));
+
+    if (response.statusCode != 200) {
+      return Response.serverError(
+          body: BusinessSendNotificationResponse(success: false, error: "Call to firebase failed"));
+    }
+
+    return Response.ok(BusinessSendNotificationResponse(success: true, error: null));
   }
 }
